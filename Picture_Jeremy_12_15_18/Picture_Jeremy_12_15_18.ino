b2dd5ff44a2828b7c17bb1b62c926f9a4b44f05b
RobMackie <rob@mackies.org>
2018-12-29 18:24:32 -0500

    Added relay and distance 	modified:   Picture_Jeremy_12_15_18/Picture_Jeremy_12_15_18.ino

--- Picture_Jeremy_12_15_18/Picture_Jeremy_12_15_18.ino
+++ Picture_Jeremy_12_15_18/Picture_Jeremy_12_15_18.ino
@@ -5,12 +5,14 @@
 #define ECHO_PIN     12  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 #define LED 4
+#define RELAY1 5
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 void setup() {
   Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
   pinMode(LED, OUTPUT);
+  pinMode(RELAY1, OUTPUT);
 }
 
 void loop() {
@@ -19,4 +21,14 @@ void loop() {
   Serial.print("Ping: ");
   Serial.print(uS / US_ROUNDTRIP_CM); // Convert ping time to distance in cm and print result (0 = outside set distance range)
   Serial.println("cm");
+  unsigned int distance = uS / US_ROUNDTRIP_CM;
+  if (distance < 35) {
+     digitalWrite(LED, HIGH);
+     digitalWrite(RELAY1, HIGH);
+  } else {
+     digitalWrite(LED, LOW);
+     digitalWrite(RELAY1, LOW);
+  }
+
+  
 }
